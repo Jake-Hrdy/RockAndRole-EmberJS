b90479467b3a3dd0613cc5abd60b591a77e13c2e
Jake Hardy <jakehardy@Jakes-MacBook-Pro.local>
2020-07-13 10:29:38 -0600

    acceptance test written, all code up-to-date on page 262 or rarwe book

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,17 +1,37 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { dasherize } from '@ember/string';
-import Band from 'rarwe/models/band';
 import { inject as service } from '@ember/service';
 
 export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', transition => {
+      if (transition.isAborted) {
+        return;
+      }
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
-  saveBand() {
-    let band = new Band({ name: this.name, slug: dasherize(this.name) })
-    this.catalog.add('band', band);
-    this.router.transitionTo('bands.band.songs', band.slug);
+  async saveBand() {
+    let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
+    this.router.transitionTo('bands.band.songs', band.id);
   }
 }
